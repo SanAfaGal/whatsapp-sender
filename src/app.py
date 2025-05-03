55ba01562b6e46a4b6efcfde48c1dc138c1d154c
SanAfaGal <afanadorgaleano@gmail.com>
2025-05-02 19:04:07 -0500

    add __init__.py files to convert folders into Python packages

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
 import tkinter as tk
-from components.app_window import WhatsAppSenderApp
+
+from src.components.app_window import WhatsAppSenderApp
+
 
 def main():
     root = tk.Tk()
     WhatsAppSenderApp(root)
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
